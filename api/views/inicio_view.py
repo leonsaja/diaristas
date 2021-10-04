8b128c77917b5a9c1ab4de4ccbf8b915a7f21e54
leonsaja <nome do email>
2021-10-03 21:08:14 -0300

    finalizado tokes e a configuração no  envio de email

--- api/views/inicio_view.py
+++ api/views/inicio_view.py
@@ -13,4 +13,8 @@ class Inicio(APIView):
         links.add_get('endereco_cep', reverse('api:endereco-cep-list'))
         links.add_get('diaristas_localidade', reverse('api:diaristas-localidades-lisst'))
         links.add_get('verificar_disponibilidade_atendimento',reverse('api:diaristas-disponibilidade-atendimento-cidade-list'))
+        links.add_post('cadastrar_usuario',reverse('api:usuario-list'))
+        links.add_post('login',reverse('token_obtain_pair'))
+        links.add_post('logout', reverse('logout-list'))
+        links.add_get('usuario_logado',reverse('api:me-list'))
         return Response({"links":links.to_array()},status=status.HTTP_200_OK)
--- ediaristas/urls.py
+++ ediaristas/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from api.views import logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('administracao/',include('administracao.urls', namespace='administracao')),
     path('api/',include('api.urls', namespace='api')),
+    path('auth/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_obtain_pair_refresh'),
+    path('auth/logout/', logout_view.Logout.as_view(), name='logout-list'),
 
 ]
